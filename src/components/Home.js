51255a0cc78a8f977f557e556956334435f50e56
Apolloon#8273 <indie.apolloon@gmail.com>
2024-09-10 00:49:35 +0800

    - Installed Bootstrap - Updated the App.css - Created NavBar component - Developing Home screen - Placeholder components

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import img1 from '../assets/images/img1.png'
+
+export default function Home() {
+    return (
+        <Container className='container-fluid' style={{ height: '100vh' }}>
+            <Row className='align-items-center justify-content-center h-100'>
+            <Col xs={12} md={6} className='d-flex justify-content-center mb-4 mb-md-0'>
+                <Image src={img1}  rounded fluid style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }} />
+            </Col>
+             <Col xs={12} md={6} className='d-flex flex-column justify-content-center text-center text-md-start'>
+             <Row>
+             <Container className='text-start'>
+             <p className='fs-1 fw-bolder'>Web Developer</p>
+             <p className='lh-base'>Hi there! I’m Janiel Apollo Bodiongan, a software developer with nearly five years experience in using a low-code 
+             development platforms like OutSystems. I specializing in building both front and back end solutions for robust web and mobile applications. Additionally, I have expanded my knowledge thru professional work or self-study in technologies like JavaScript, PostgreSQL, ReactJS, SASS and Google Cloud.</p>
+             </Container>  
+             </Row>
+                
+            </Col>
+            </Row>
+        </Container>
+    );
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import {Nav, Container } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function Navigation(){
+    return(
+      <Navbar bg="dark" data-bs-theme="dark" className="sticky-md-top" style={{ boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
+      <Container>
+        <Nav className="mx-auto">
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#work">Work</Nav.Link>
+          <Nav.Link href="#process">Process</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+    );
+}
+
