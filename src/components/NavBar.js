96b440c29668a77398bc1073a16371d1db56a862
Apolloon#8273 <indie.apolloon@gmail.com>
2024-09-12 03:04:59 +0800

    Updated the package and navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,24 @@
 import {Nav, Container } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
+import { useEffect } from 'react';
 
 export default function Navigation(){
-    const gitUrl = 'https://github.com/janielapollo';
+  const gitUrl = 'https://github.com/janielapollo';
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      const message = "Are you sure you want to leave this page?";
+      event.returnValue = message; 
+      return message;
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+    
     return(
       <Navbar bg="dark" data-bs-theme="dark" className="sticky-md-top" style={{ boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
       <Container>
